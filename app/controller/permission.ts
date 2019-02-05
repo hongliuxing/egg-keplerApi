3e01eb24f047153337ffb619656f26946413dc89
wscart <wscart@qq.com>
2019-02-05 22:24:07 +0800

    纠正

--- app/controller/permission.ts
+++ app/controller/permission.ts
@@ -12,13 +12,12 @@ export default class UserPermissionController extends Controller {
   }
   public async create() {
     const { ctx } = this;
-    const permission = await this.service.permission.create({});
-    const data: any = permission;
+    const data = await this.service.permission.create(ctx.params);
     ctx.body = data;
   }
-  public async update(params: any) {
+  public async update() {
     const { ctx } = this;
-    const data = await this.service.permission.update(params);
+    const data = await this.service.permission.update(ctx.params);
     ctx.body = data;
   }
 }
